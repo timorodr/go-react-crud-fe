e617ed5cac1239dedb5ad8462b6d126e9f43fa85
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-04-08 21:04:56 -0500

    add and delete functions created

--- src/components/entries.components.js
+++ src/components/entries.components.js
@@ -2,3 +2,46 @@ import React, {useState, useEffect} from "react";
 import axios from "axios"
 import {Button, Form, Container, Modal} from 'react-bootstrap'
 import Entry from './single-entry.component'
+
+const Entries = () => {
+    return(
+        <div>
+            <Container>
+                <Button onClick={() => setAddNewEntry(true)}>Track today's calories</Button>
+            </Container>
+            <Container>
+            {entries != null && entries.map((entry, i) => (
+                <Entry entryData={entry} deleteSingleEntry={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry}/>
+            ))}
+            </Container>
+        </div>
+    )
+}
+
+// call api's
+
+function addSingleEntry(){
+    setAddNewEntry(false)
+    let URL = "http://localhost:8000/entry/create"
+    axios.post(URL, {
+        "ingredients": newEntry.ingredients,
+        "dish": newEntry.dish,
+        "calories": newEntry.calories,
+        "fat": parseFloat(newEntry.fat)
+    }).then(response => {
+        if(response.status == 200){
+            setFreshData(true)
+        }
+    })
+}
+
+function deleteSingleEntry(id){
+    let URL = "http://localhost:8000/entry/delete" + id
+    axios.delete(URL, {
+
+    }).then(response => {
+        if(response.status == 200){
+            setFreshData(true)
+        }
+    })
+}
